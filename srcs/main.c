2a61df04038475655e3ccef6ff1f6de392094f4c
Jino <ntoniolo@student.42.fr>
2018-01-08 23:18:39 +0100

    work with COPY PAST Hello window

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,88 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/08 16:25:46 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/01/08 22:48:45 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/01/08 23:18:16 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int			main(int argc, char **argv)
 {
 	t_env	e;
 	int		index;
 
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		ft_printf("AA");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		ft_printf("BB");
+		return -1;
+	}
+
+
+	while (!glfwWindowShouldClose(window))
+	    {
+	        // input
+	        // -----
+	        processInput(window);
+
+	        // render
+	        // ------
+	        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	        glClear(GL_COLOR_BUFFER_BIT);
+
+	        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+	        // -------------------------------------------------------------------------------
+	        glfwSwapBuffers(window);
+	        glfwPollEvents();
+	    }
+		// glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+
+
+
 	ft_bzero(&e, sizeof(t_env));
 	index = flag(&e.flag, argc, argv);
 	scop_loop(&e);
 	end_of_program(&e, NULL, 0);
 	return (0);
+}
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+	(void)window;
+    glViewport(0, 0, width, height);
 }
