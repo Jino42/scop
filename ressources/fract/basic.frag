5a0a0988000fb873931b0a25cc182df2691974e4
Jino <ntoniolo@student.42.fr>
2018-02-10 00:23:59 +0100

    Start 3d rendering

--- ressources/fract/basic.frag
+++ ressources/fract/basic.frag
@@ -2,30 +2,55 @@
 
 #define EPSILON 0.00005f
 
+struct t_material
+{
+	vec3	ambient;
+	vec3	diffuse;
+	vec3	specular;
+	float	shininess;
+	float	transparency;
+	int		texture_ambient;
+	int		texture_diffuse;
+	int		texture_specular;
+	int		texture_shininess;
+	int		texture_normal;
+	int		flag;
+};
+
+struct t_light
+{
+	vec3	position;
+	vec3	ambient;
+	vec3	diffuse;
+	vec3	specular;
+};
+
 float DE(vec3 pos) {
-   float Power = 8.0;
-    vec3 z = pos;
+	float Power = 8.0;
+	vec3 z = pos;
     float dr = 1.0;
     float r = 0.0;
-    for (int i = 0; i < 50 ; i++) {
+    for (int i = 0; i < 10 ; i++)
+	{
         r = length(z);
-        if (r>4.0) break;
+        if (r > 4.0)
+			break;
 
         // convert to polar coordinates
-        float theta = acos(z.z/r);
+        float theta = acos(z.z / r);
         float phi = atan(z.y,z.x);
-        dr =  pow( r, Power-1.0)*Power*dr + 1.0;
+        dr =  pow(r, Power - 1.0) * Power * dr + 1.0;
 
         // scale and rotate the point
-        float zr = pow( r,Power);
-        theta = theta*Power;
-        phi = phi*Power;
+        float zr = pow(r, Power);
+        theta = theta * Power;
+        phi = phi * Power;
 
         // convert back to cartesian coordinates
-        z = zr*vec3(sin(theta)*cos(phi), sin(phi)*sin(theta), cos(theta));
-        z+=pos;
+        z = zr * vec3(sin(theta) * cos(phi), sin(phi) * sin(theta), cos(theta));
+        z += pos;
     }
-    return 0.5*log(r)*r/dr;
+    return (0.5 * log(r) * r / dr);
 }
 
 vec2    rotate(vec2 pos, float angle)
@@ -55,16 +80,17 @@ float     map(vec3 pos)
 {
    //pos = vec3(rotate(pos.yx, pos.z * cos(iTime) * 0.02), pos.z);
   // pos = vec3(pos.x, rotate(pos.yz, pos.x * cos(iTime) * 0.02));
-   float planeDist = intersect_plane(pos);
+   //float planeDist = intersect_plane(pos);
 
 
    //pos = vec3(rotate(pos.xy, pos.z * cos(iTime) * 0.02), pos.z);
 
-   pos = mod(vec3(pos.x, pos.y, pos.z) + 10.0, 20.0) - 10.0;
+   //pos = mod(vec3(pos.x, pos.y, pos.z) + 10.0, 20.0) - 10.0;
 
    return DE(pos);//min(planeDist, intersect_box(pos, vec3(2, 2, 2)));
 }
 
+uniform t_light		light;
 out vec4 fragColor;
 in vec2 uv;
 in vec3 position;
@@ -96,8 +122,9 @@ void    main()
   //vec3 dir = vec3(vec4(camDi, 1.0) * oV);
   vec3 dir = vec3(vec4(tmp, 1.0) * nV);
    vec4 color = vec4(0.4f, 0.4f, 0.4f, 0.0f);
-   vec3 pos = vec3(vec3(0.,0.,-2.) + cameraPosition);
-   for (int i = 0; i < 64; i++)
+   vec3 pos = cameraPosition;
+   lightDir = position - light.position;
+   for (int i = 0; i < 50; i++)
    {
        float d = map(pos);
        if (d < 0.01f)
@@ -105,12 +132,12 @@ void    main()
            //color = material(pos);
         	vec2 Dd = vec2(0.01, 0.0);
            	vec3 n = normalize(vec3(DE(pos+Dd.xyy)-DE(pos-Dd.xyy),
-                                DE(pos+Dd.yxy)-DE(pos-Dd.yxy),
+                                	DE(pos+Dd.yxy)-DE(pos-Dd.yxy),
                                     DE(pos+Dd.yyx)-DE(pos-Dd.yyx)));
            //float fc = fract(length(pos - camPos));
            //color = vec3(fc, fc, fc);
-           color = vec4(0.25, 0.5, 0.25, 1.) * dot(n, -dir);
-           break;
+           color = vec4(0.25, 0.5, 1.0 / 64.0 * i, 1.) * dot(n, -lightDir);
+           break ;
        }
        pos += d * dir;
    }
--- srcs/render_loop.c
+++ srcs/render_loop.c
@@ -6,11 +6,32 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 21:59:19 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/02/09 00:08:27 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/02/10 00:23:04 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.h"
+
+t_texture			*texture_3construct()
+{
+	t_texture			*texture;
+
+	if (!(texture = ft_memalloc(sizeof(t_texture))))
+		return (NULL);
+	unsigned char *texels = ft_memalloc(WIDTH * HEIGHT * 10 * 4);
+	for(int i = 0; i < WIDTH * HEIGHT * 10; i++)
+		texels[i] = 255;
+	glGenTextures(1, &texture->id);
+	glBindTexture(GL_TEXTURE_3D, texture->id);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+	glTexImage3D(GL_TEXTURE_3D, 0, GL_RGB8, WIDTH, HEIGHT, 10, 0, GL_RGB,
+	             GL_UNSIGNED_BYTE, texels);
+	return (texture);
+}
 
 void	matrix_scaling(t_matrix *m, const float s)
 {
@@ -64,11 +85,67 @@ bool	render_loop(t_env *e, const char **argv, t_glfw *glfw)
 
 	t_shader *shader_l  = shader_construct("ressources/cube/basic.vert",
 										"ressources/cube/basic.frag");
-				t_shader *shader_f  = shader_construct("ressources/fract/basic.vert",
-													"ressources/fract/basic.frag");
+	t_shader *shader_f  = shader_construct("ressources/fract/basic.vert",
+										"ressources/fract/basic.frag");(void)shader_f;
+	t_shader *shader_g  = shader_construct("ressources/tex3/cube/basic.vert",
+										"ressources/tex3/cube/basic.frag");(void)shader_g;
 
-	t_matrix projection = matrix_get_projection_opengl(66.f, (float)WIDTH / (float)HEIGHT, 0.1f, 100.f);
+
+	t_matrix projection = matrix_get_projection_opengl(66.f, (float)WIDTH / (float)HEIGHT, 0.01f, 100.f);
 	t_matrix view = matrix_view(&e->cam);
+
+	t_texture *tex3d = texture_3construct();
+
+
+
+	glEnable(GL_BLEND);
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	(void)tex3d;
+	float vertices[] = {
+        // positions          // colors           // texture coords
+        0.f, 0.f, 0.f,  0.f, 0.f, 0.f,
+        0.f, 0.f, 1.f,  0.f, 0.f, 1.f,
+        0.f, 1.f, 0.f,  0.f, 1.f, 0.f,
+    	0.f, 1.f, 1.f,  0.f, 1.f, 1.f,
+		1.f, 0.f, 0.f,  1.f, 0.f, 0.f,
+		1.f, 0.f, 1.f,  1.f, 0.f, 1.f,
+	   	1.f, 1.f, 0.f,  1.f, 1.f, 0.f,
+	   	1.f, 1.f, 1.f,  1.f, 1.f, 1.f,
+    };
+	unsigned int indices[] = {
+        0, 6, 4, // first triangle
+		0, 6, 2,  // second triangle
+		0, 3, 2,  // second triangle
+        0, 1, 3,  // second triangle
+		2, 7, 6,
+		2, 3, 7,
+		4, 6, 7,
+		4, 7, 5,
+		0, 4, 5,
+		0, 5, 1,
+		1, 5, 7,
+		1, 7, 3
+    };
+	unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    // texture coord attribute
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
 	glEnable(GL_DEPTH_TEST);
 	glPointSize(5.0);
@@ -94,7 +171,62 @@ bool	render_loop(t_env *e, const char **argv, t_glfw *glfw)
 		view = matrix_view(&e->cam);
 		model_render(model, &e->cam, light, &view, &projection, shader);
 		model_render(obj_light, &e->cam, light, &view, &projection, shader_l);
-		model_render(obj_f, &e->cam, light, &view, &projection, shader_f);
+		//model_render(obj_f, &e->cam, light, &view, &projection, shader_f);
+
+
+
+        // render container
+        shader_g->use(shader_g);
+		t_matrix temp = matrix_get_mult_matrix(&model->transform, &view);
+		t_matrix mvp = matrix_get_mult_matrix(&temp, &projection);
+		glUniform3fv(
+				glGetUniformLocation(shader_g->program, "light.ambient"),
+				1,
+				(GLfloat *)&light->ambient);
+		glUniform3fv(
+				glGetUniformLocation(shader_g->program, "light.diffuse"),
+				1,
+				(GLfloat *)&light->diffuse);
+		glUniform3fv(
+				glGetUniformLocation(shader_g->program, "light.specular"),
+				1,
+				(GLfloat *)&light->specular);
+		glUniform3fv(
+				glGetUniformLocation(shader_g->program, "light.position"),
+				1,
+				(GLfloat *)&light->position);
+		glUniform3f(
+				glGetUniformLocation(shader_g->program, "cameraPosition"),
+				e->cam.position.x,
+				e->cam.position.y,
+				e->cam.position.z);
+		glUniform3f(
+				glGetUniformLocation(shader_g->program, "camDir"),
+				e->cam.to.x,
+				e->cam.to.y,
+				e->cam.to.z);
+		glUniformMatrix4fv(
+				glGetUniformLocation(shader_g->program, "MVP"),
+				1, GL_FALSE, &mvp.matrix[0][0]);
+		glUniformMatrix4fv(
+				glGetUniformLocation(shader_g->program, "V"),
+				1, GL_FALSE, &view.matrix[0][0]);
+		glUniformMatrix4fv(
+				glGetUniformLocation(shader_g->program, "P"),
+				1, GL_FALSE, &projection.matrix[0][0]);
+		glUniformMatrix4fv(
+				glGetUniformLocation(shader_g->program, "M"),
+				1, GL_FALSE, &model->transform.matrix[0][0]);
+				glUniform1i(glGetUniformLocation(shader_g->program, "tex3d"), 0);
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_3D, tex3d->id);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 12*3, GL_UNSIGNED_INT, 0);
+
+
+
+
+
 		glfwSwapBuffers(glfw->window);
 		glfwPollEvents();
 	}
