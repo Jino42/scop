a6d09629a5908b0349fa9ed967c1281883758e3b
Jino <ntoniolo@student.42.fr>
2018-02-10 19:22:03 +0100

    remove fcking fractal

--- srcs/render_loop.c
+++ srcs/render_loop.c
@@ -6,7 +6,7 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 21:59:19 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/02/10 00:23:04 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/02/10 19:21:42 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,9 +59,6 @@ bool	render_loop(t_env *e, const char **argv, t_glfw *glfw)
 							NULL);
 								//"ressources/Orange_obj/Color.rgb");
 
-//	if (argv[3])
-//		if (!parsing_mtl(model, argv[3]))
-//			return (0);
 	t_shader *shader  = shader_construct("shader/basic.vert",
 										"shader/basic.frag");
 	matrix_scaling(&model->transform, 0.005f);
@@ -73,18 +70,15 @@ bool	render_loop(t_env *e, const char **argv, t_glfw *glfw)
 	t_model *obj_light = model_construct("ressources/cube/",
 										"ressources/cube/cube.obj",
 											NULL);
-	t_model *obj_f = model_construct("ressources/fract/",
-										"ressources/fract/cub.obj",
-											NULL);
-	if(!obj_f)
-		exit(0);
 	t_vector move = vector_construct(0.3f, 0.3f, 0.3f);
 	matrix_translation(&obj_light->transform, &move);
 	matrix_transpose(&obj_light->transform);
 	matrix_scaling(&obj_light->transform, 0.05f);
 
+	//matrix_scaling(&obj_light2->transform, 1.f);
+
 	t_shader *shader_l  = shader_construct("ressources/cube/basic.vert",
-										"ressources/cube/basic.frag");
+										"ressources/cube/basic.frag");(void)shader_l;
 	t_shader *shader_f  = shader_construct("ressources/fract/basic.vert",
 										"ressources/fract/basic.frag");(void)shader_f;
 	t_shader *shader_g  = shader_construct("ressources/tex3/cube/basic.vert",
@@ -94,58 +88,9 @@ bool	render_loop(t_env *e, const char **argv, t_glfw *glfw)
 	t_matrix projection = matrix_get_projection_opengl(66.f, (float)WIDTH / (float)HEIGHT, 0.01f, 100.f);
 	t_matrix view = matrix_view(&e->cam);
 
-	t_texture *tex3d = texture_3construct();
-
-
 
 	glEnable(GL_BLEND);
-glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	(void)tex3d;
-	float vertices[] = {
-        // positions          // colors           // texture coords
-        0.f, 0.f, 0.f,  0.f, 0.f, 0.f,
-        0.f, 0.f, 1.f,  0.f, 0.f, 1.f,
-        0.f, 1.f, 0.f,  0.f, 1.f, 0.f,
-    	0.f, 1.f, 1.f,  0.f, 1.f, 1.f,
-		1.f, 0.f, 0.f,  1.f, 0.f, 0.f,
-		1.f, 0.f, 1.f,  1.f, 0.f, 1.f,
-	   	1.f, 1.f, 0.f,  1.f, 1.f, 0.f,
-	   	1.f, 1.f, 1.f,  1.f, 1.f, 1.f,
-    };
-	unsigned int indices[] = {
-        0, 6, 4, // first triangle
-		0, 6, 2,  // second triangle
-		0, 3, 2,  // second triangle
-        0, 1, 3,  // second triangle
-		2, 7, 6,
-		2, 3, 7,
-		4, 6, 7,
-		4, 7, 5,
-		0, 4, 5,
-		0, 5, 1,
-		1, 5, 7,
-		1, 7, 3
-    };
-	unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    // texture coord attribute
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	glEnable(GL_DEPTH_TEST);
 	glPointSize(5.0);
@@ -171,61 +116,6 @@ glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		view = matrix_view(&e->cam);
 		model_render(model, &e->cam, light, &view, &projection, shader);
 		model_render(obj_light, &e->cam, light, &view, &projection, shader_l);
-		//model_render(obj_f, &e->cam, light, &view, &projection, shader_f);
-
-
-
-        // render container
-        shader_g->use(shader_g);
-		t_matrix temp = matrix_get_mult_matrix(&model->transform, &view);
-		t_matrix mvp = matrix_get_mult_matrix(&temp, &projection);
-		glUniform3fv(
-				glGetUniformLocation(shader_g->program, "light.ambient"),
-				1,
-				(GLfloat *)&light->ambient);
-		glUniform3fv(
-				glGetUniformLocation(shader_g->program, "light.diffuse"),
-				1,
-				(GLfloat *)&light->diffuse);
-		glUniform3fv(
-				glGetUniformLocation(shader_g->program, "light.specular"),
-				1,
-				(GLfloat *)&light->specular);
-		glUniform3fv(
-				glGetUniformLocation(shader_g->program, "light.position"),
-				1,
-				(GLfloat *)&light->position);
-		glUniform3f(
-				glGetUniformLocation(shader_g->program, "cameraPosition"),
-				e->cam.position.x,
-				e->cam.position.y,
-				e->cam.position.z);
-		glUniform3f(
-				glGetUniformLocation(shader_g->program, "camDir"),
-				e->cam.to.x,
-				e->cam.to.y,
-				e->cam.to.z);
-		glUniformMatrix4fv(
-				glGetUniformLocation(shader_g->program, "MVP"),
-				1, GL_FALSE, &mvp.matrix[0][0]);
-		glUniformMatrix4fv(
-				glGetUniformLocation(shader_g->program, "V"),
-				1, GL_FALSE, &view.matrix[0][0]);
-		glUniformMatrix4fv(
-				glGetUniformLocation(shader_g->program, "P"),
-				1, GL_FALSE, &projection.matrix[0][0]);
-		glUniformMatrix4fv(
-				glGetUniformLocation(shader_g->program, "M"),
-				1, GL_FALSE, &model->transform.matrix[0][0]);
-				glUniform1i(glGetUniformLocation(shader_g->program, "tex3d"), 0);
-				glActiveTexture(GL_TEXTURE0);
-				glBindTexture(GL_TEXTURE_3D, tex3d->id);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 12*3, GL_UNSIGNED_INT, 0);
-
-
-
-
 
 		glfwSwapBuffers(glfw->window);
 		glfwPollEvents();
