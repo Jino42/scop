d48767f7f2ec90c85b22299d8f4ba9bcfb16d93a
Jino <ntoniolo@student.42.fr>
2018-07-18 19:49:10 +0200

    Path path model

--- srcs/c_model/model.c
+++ srcs/c_model/model.c
@@ -6,7 +6,7 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 16:46:40 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/07/17 16:47:23 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/07/18 19:48:46 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 t_model		*model_construct(const char *path, const char *name)
 {
 	t_model		*model;
+	char		*last_slash;
 
 	printf("%s %s\n", path, name);
 	if (!(model = ft_memalloc(sizeof(t_model))))
@@ -23,8 +24,20 @@ t_model		*model_construct(const char *path, const char *name)
 	model->type_draw = GL_FILL;
 	if (!(model->m_mesh = m_mesh_construct()))
 		return (model_destruct(&model));
-	model->name = strdup(name);
-	model->path = strdup(path);
+	if (!(model->name = strdup(name)))
+		return (model_destruct(&model));
+	last_slash = strrchr(path, '/');
+	if (last_slash)
+	{
+		if (!(model->path = ft_strnew(-(path - last_slash))))
+			return (model_destruct(&model));
+		strncpy(model->path, path, -(path - last_slash) + 1);
+	}
+	else
+	{
+		if (!(model->path = strdup("./")))
+			return (model_destruct(&model));
+	}
 	model->scaling = vector_construct(1.f, 1.f, 1.f);
 	model->update = true;
 	model->same_scaling = 1;
