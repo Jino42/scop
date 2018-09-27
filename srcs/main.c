3cfe7103e990b404d25ae70029e00a1b37e598a6
Jino <ntoniolo@student.42.fr>
2018-09-28 00:02:23 +0200

    usage

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/19 00:40:01 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/09/19 00:41:34 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/09/28 00:02:15 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ int			main(int argc, char **argv)
 {
 	t_env *e;
 
+	if (argc != 2)
+	{
+		dprintf(2, "usage: ./scop file.json\n");
+		return (0);
+	}
 	if (!(e = env_construct(argc, argv)))
 	{
 		return (ft_bool_error("Erreur: L'initialisation de t_env a échoué",
