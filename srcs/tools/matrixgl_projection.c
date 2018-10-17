64a6eb959752a8670235d1bc5f28e0c7a8f78523
Jino <ntoniolo@student.42.fr>
2018-10-17 14:17:22 +0200

    update

--- srcs/tools/matrixgl_projection.c
+++ srcs/tools/matrixgl_projection.c
@@ -6,7 +6,7 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/15 20:51:03 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/09/19 00:01:06 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/10/17 14:11:06 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,14 @@ t_matrix	matrixgl_get_projection(const float fov,
 	t_matrix	m;
 	float		scale;
 	float		r;
-	float		t;
 
 	bzero(&m, sizeof(t_matrix));
 	scale = tanf(fov * 0.5f * M_PI / 180.0f) * near;
 	r = ratio * scale;
-	t = scale;
 	m.matrix[0][0] = 2.f * near / (r - -r);
-	m.matrix[1][1] = 2.f * near / (t - -t);
+	m.matrix[1][1] = 2.f * near / (scale - -scale);
 	m.matrix[2][0] = (r + -r) / (r - -r);
-	m.matrix[2][1] = (t + -t) / (t - -t);
+	m.matrix[2][1] = (scale + -scale) / (scale - -scale);
 	m.matrix[2][2] = -(far + near) / (far - near);
 	m.matrix[2][3] = -1.f;
 	m.matrix[3][2] = -2.f * far * near / (far - near);
--- srcs/tools/matrixgl_view.c
+++ srcs/tools/matrixgl_view.c
@@ -6,7 +6,7 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/15 20:51:59 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/09/19 00:13:26 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/10/17 14:13:12 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_matrix		look_at_gl(const t_vector *position,
 	t_vector zaxis;
 	t_vector xaxis;
 	t_vector yaxis;
-	t_matrix rotation;
+	t_matrix look_at;
 	t_matrix translation;
 
 	zaxis = vector_get_sub(position, to);
@@ -36,17 +36,17 @@ t_matrix		look_at_gl(const t_vector *position,
 	xaxis = vector_get_cross_product(up, &zaxis);
 	vector_normalize(&xaxis);
 	yaxis = vector_get_cross_product(&zaxis, &xaxis);
-	rotation = matrix_get_identity();
+	look_at = matrix_get_identity();
 	translation = matrix_get_identity();
-	rotation.matrix[0][0] = xaxis.x;
-	rotation.matrix[1][0] = xaxis.y;
-	rotation.matrix[2][0] = xaxis.z;
-	rotation.matrix[0][1] = yaxis.x;
-	rotation.matrix[1][1] = yaxis.y;
-	rotation.matrix[2][1] = yaxis.z;
-	rotation.matrix[0][2] = zaxis.x;
-	rotation.matrix[1][2] = zaxis.y;
-	rotation.matrix[2][2] = zaxis.z;
+	look_at.matrix[0][0] = xaxis.x;
+	look_at.matrix[1][0] = xaxis.y;
+	look_at.matrix[2][0] = xaxis.z;
+	look_at.matrix[0][1] = yaxis.x;
+	look_at.matrix[1][1] = yaxis.y;
+	look_at.matrix[2][1] = yaxis.z;
+	look_at.matrix[0][2] = zaxis.x;
+	look_at.matrix[1][2] = zaxis.y;
+	look_at.matrix[2][2] = zaxis.z;
 	*((t_vector3f *)&translation.matrix[3][0]) = *((t_vector3f *)position);
-	return (matrix_get_mult_matrix(&translation, &rotation));
+	return (matrix_get_mult_matrix(&translation, &look_at));
 }
